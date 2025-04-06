d6df695f91cffecb54bb7cbe9321ae4ddb31c2ba
Matheus Spinosa <spinosa665@gmail.com>
2025-04-06 20:53:25 -0300

    feat: create auth middleware

--- src/@types/express/index.d.ts
+++ src/@types/express/index.d.ts
@@ -2,10 +2,7 @@
 declare namespace Express {
   // eslint-disable-next-line @typescript-eslint/naming-convention
   export interface Request {
-    customer?: {
-      id: number;
-      data?: any;
-    };
+    customer?: any;
     queryRunner?: {
       id: string;
       queryRunner: any;
